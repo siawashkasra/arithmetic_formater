06dec29bf828ed5b34f0138f1cdbdfd3103917b1
siawashkasra <siawashkasra@gmail.com>
2020-11-26 22:37:06 +0430

    Add dashes at the end of problems

--- arithmetic_formater.py
+++ arithmetic_formater.py
@@ -1,7 +1,7 @@
 def arithmetic_arranger(problems):
     header = ''
     bottom = ''
-    dashes = ''
+    dashes = list()
     arranged = list()
     problems_list = map(lambda x: x.split(" "), problems)
     for problem in problems_list:
@@ -9,20 +9,23 @@ def arithmetic_arranger(problems):
             for align, p in zip('<', problem):
                 header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[2]) + 1), fill='', align=align, width= 2 + len(problem[2]) + 4)
                 bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2], fill="", align=align, width= 2 + len(problem[2]) + 4)
-                dashes = (2 + len(problem[2])) * '-'
+            dashes.append((2 + len(problem[2])) * '-')
+                
         if len(problem[0]) == len(problem [2]):
             for align, p in zip('<', problem):
                 header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[0]) + 1), fill='', align=align, width= 2 + len(problem[0]) + 4)
                 bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2], fill="", align=align, width= 2 + len(problem[0]) + 4)
-                dashes = (2 + len(problem[0])) * '-'
+            dashes.append((2 + len(problem[0])) * '-')
         if len(problem[0]) > len(problem[2]):
             for align, p in zip('<', problem):
                 header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[0]) + 1), fill="", align=align, width= 2 + len(problem[0]) + 4)
                 bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2].rjust(len(problem[0])), fill="", align=align, width= 2 + len(problem[0]) + 4)
-                dashes = (2 + len(problem[2])) * '-'
+            dashes.append((2 + len(problem[0])) * '-')
 
     header += header.join("\n")
     bottom += bottom.join("\n")
-    arranged.append(header + bottom + dashes)
+    dashes = map(lambda x: x.ljust(len(x) + 4), dashes)
+    f_dashes = "".join(list(dashes))
+    arranged.append(header + bottom + f_dashes)
     return arranged[0]
-print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
