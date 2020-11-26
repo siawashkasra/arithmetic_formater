1ba1077b957e9090770dda3aa115eec4792c4bdf
siawashkasra <siawashkasra@gmail.com>
2020-11-26 21:43:26 +0430

    Improve arithmetic format for variable lenght problems

--- arithmetic_formater.py
+++ arithmetic_formater.py
@@ -7,21 +7,22 @@ def arithmetic_arranger(problems):
     for problem in problems_list:
         if len(problem[0]) < len(problem [2]):
             for align, p in zip('<', problem):
-                header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[2]) + 1), fill='*', align=align, width= 2 + len(problem[2]) + 2)
-                bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2], fill="*", align=align, width=2 + len(problem[2]) + 2)
-                dashes = len(bottom) * '-'
-        # if len(problem[0]) == len(problem [2]):
-        #     for align, p in zip('<', problem):
-        #         header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[0]) + 1), fill='*', align=align, width= 2 + len(problem[0]) + 2)
-        #         bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2], fill="*", align=align, width=2 + len(problem[0]) + 2)
-                dashes = len(bottom) * '-'
-        # if len(problem[0]) > len(problem[2]):
-        #     for align, p in zip('<', problem):
-        #         header += '{0:{fill}{align}{width}}'.format('{0:{fill}{align}{width}}'.format(p, fill='*', align='>', width=len(problem[0]) + 1), fill=align, align=align, width=(len(p) + 4 + len(problem[1])))
-        #         bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + '{0:{fill}{align}{width}}'.format(problem[2], fill='*', align='>', width=len(problem[0]) - len(problem[2])), fill=align, align=align, width=(len(problem[2]) + 4 + len(problem[1]) + 1))
-                
+                header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[2]) + 1), fill='', align=align, width= 2 + len(problem[2]) + 4)
+                bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2], fill="", align=align, width= 2 + len(problem[2]) + 4)
+                dashes = (2 + len(problem[2])) * '-'
+        if len(problem[0]) == len(problem [2]):
+            for align, p in zip('<', problem):
+                header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[0]) + 1), fill='', align=align, width= 2 + len(problem[0]) + 4)
+                bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2], fill="", align=align, width= 2 + len(problem[0]) + 4)
+                dashes = (2 + len(problem[0])) * '-'
+        if len(problem[0]) > len(problem[2]):
+            for align, p in zip('<', problem):
+                header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[0]) + 1), fill="", align=align, width= 2 + len(problem[0]) + 4)
+                bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2].rjust(len(problem[0])), fill="", align=align, width= 2 + len(problem[0]) + 4)
+                dashes = (2 + len(problem[2])) * '-'
+
     header += header.join("\n")
     bottom += bottom.join("\n")
     arranged.append(header + bottom + dashes)
     return arranged[0]
-print(arithmetic_arranger(["32 + 546", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["32 + 8", "1 - 3801", "9999 + 9999", "523 - 49"]))
