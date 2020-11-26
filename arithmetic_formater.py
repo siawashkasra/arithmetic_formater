e35bfe7363cda5f5578c1d0697944ab362546d69
siawashkasra <siawashkasra@gmail.com>
2020-11-26 20:56:18 +0430

    Add arithmetic format logic for variable length problems

--- arithmetic_formater.py
+++ arithmetic_formater.py
@@ -5,14 +5,15 @@ def arithmetic_arranger(problems):
     arranged = list()
     problems_list = map(lambda x: x.split(" "), problems)
     for problem in problems_list:
-        # if len(problem[0]) < len(problem [2]):
-        #     for align, p in zip('<', problem):
-        #         header += '{0:{fill}{align}{width}}'.format('{0:{fill}{align}{width}}'.format(p, fill='*', align='>', width=len(problem[2]) + len(p)), fill=align, align=align, width=(len(p) + 4 + len(problem[2])))
-        #         bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2], fill=align, align=align, width=(len(problem[2]) + 4 + len(problem[1]) + 1))
-        if len(problem[0]) == len(problem [2]):
+        if len(problem[0]) < len(problem [2]):
             for align, p in zip('<', problem):
-                header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[0]) + 1), fill='*', align=align, width= 2 + len(problem[0]) + 2)
-                bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2], fill="*", align=align, width= 2 + len(problem[0]) + 2)
+                header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[2]) + 1), fill='*', align=align, width= 2 + len(problem[2]) + 2)
+                bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2], fill="*", align=align, width=2 + len(problem[2]) + 2)
+                dashes = len(bottom) * '-'
+        # if len(problem[0]) == len(problem [2]):
+        #     for align, p in zip('<', problem):
+        #         header += '{0:{fill}{align}{width}}'.format(problem[0].rjust(len(problem[1]) + len(problem[0]) + 1), fill='*', align=align, width= 2 + len(problem[0]) + 2)
+        #         bottom += '{0:{fill}{align}{width}}'.format(problem[1].ljust(len(problem[1]) + 1) + problem[2], fill="*", align=align, width=2 + len(problem[0]) + 2)
                 dashes = len(bottom) * '-'
         # if len(problem[0]) > len(problem[2]):
         #     for align, p in zip('<', problem):
@@ -23,4 +24,4 @@ def arithmetic_arranger(problems):
     bottom += bottom.join("\n")
     arranged.append(header + bottom + dashes)
     return arranged[0]
-print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
+print(arithmetic_arranger(["32 + 546", "3801 - 2", "45 + 43", "123 + 49"]))
